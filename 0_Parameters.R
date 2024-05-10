be85279310d543f9837571f94ec539869c95f028
Robin Blythe <robin.blythe@qut.edu.au>
2024-05-10 17:13:10 +1200

    update params

--- 0_Parameters.R
+++ 0_Parameters.R
@@ -1,15 +1,44 @@
 #Enter parameters for original study
-prevalence <- list(
-  HA_cemented = 384+292,
-  HA_cementless = 20+45,
-  THA_cemented = 718,
-  THA_cementless = 335
+prevalence <- list( #unipolar + bipolar
+  age_65 = list(
+    HA_cemented = 384+292,
+    HA_cementless = 20+45,
+    THA_cemented = 718,
+    THA_cementless = 335),
+  
+  age_75 = list(
+    HA_cemented = 1142+685,
+    HA_cementless = 64+115,
+    THA_cemented = 491,
+    THA_cementless = 177
+  ),
+  
+  age_85 = list(
+    HA_cemented = 1598+761,
+    HA_cementless = 83+121,
+    THA_cemented = 163,
+    THA_cementless = 64
+  )
 )
 
-#Weights for original study analysis
-HA_weight_cemented <- c(384/676, 292/676)
-HA_weight_cementless <- c(20/65, 45/65)
+#Weights for original study analysis - can discard when new data arrives
+weights <- list(
+  age_65 = list(
+    cemented = c(384/676, 292/676),
+    cementless = c(20/65, 45/65)
+  ),
+  age_75 = list(
+    cemented = c(1142/1827, 685/1827),
+    cementless = c(64/179, 115/179)
+  ),
+  age_85 = list(
+    cemented = c(1598/2359, 761/2359),
+    cementless = c(83/204, 121/204)
+  )
+)
 
+#Note that Beta is bounded (0,1)
+#For values at or near 0, use an approximation: Beta(0, 100)
 transitions <- list(
   
   death = list(
@@ -69,100 +98,150 @@ transitions <- list(
   
   revision = list(
     age_65 = list(
-      HA_cemented = list( #Defined as unipolar[1] bipolar[2]
-        year1 = rbeta(iter, 101, 3789)*HA_weight_cemented[1] + rbeta(iter, 76, 2727)*HA_weight_cemented[2],
-        year2 = rbeta(iter, 30, 1752)*HA_weight_cemented[1] + rbeta(iter, 17, 985)*HA_weight_cemented[2],
-        year3 = rbeta(iter, 19, 1120)*HA_weight_cemented[1] + rbeta(iter, 5, 464)*HA_weight_cemented[2],
-        year4 = rbeta(iter, 14, 738)*HA_weight_cemented[1] + rbeta(iter, 1, 168)*HA_weight_cemented[2],
-        year5 = rbeta(iter, 6, 449)*HA_weight_cemented[1] + rbeta(iter, 3, 307)*HA_weight_cemented[2],
+      HA_cemented = list( #Defined as unipolar[1] + bipolar[2]
+        year1 = rbeta(iter, 101, 3789)*weights$age_65$cemented[1] + rbeta(iter, 76, 2727)*weights$age_65$cemented[2],
+        year2 = rbeta(iter, 30, 1752)*weights$age_65$cemented[1] + rbeta(iter, 17, 985)*weights$age_65$cemented[2],
+        year3 = rbeta(iter, 19, 1121)*weights$age_65$cemented[1] + rbeta(iter, 5, 464)*weights$age_65$cemented[2],
+        year4 = rbeta(iter, 14, 738)*weights$age_65$cemented[1] + rbeta(iter, 1, 168)*weights$age_65$cemented[2],
+        year5 = rbeta(iter, 5, 449)*weights$age_65$cemented[1] + rbeta(iter, 3, 307)*weights$age_65$cemented[2]
       ),
       HA_cementless = list(
-        year1 = rbeta(iter, 33, 892)*HA_weight_cemented[1] + rbeta(iter, 29, 615)*HA_weight_cemented[2],
-        year2 = rbeta(iter, 10, 369)*HA_weight_cemented[1] + rbeta(iter, 76, 2727)*HA_weight_cemented[2],
-        year3 = rbeta(iter, 4, 204)*HA_weight_cemented[1] + rbeta(iter, 0, 31)*HA_weight_cemented[2],
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 33, 892)*weights$age_65$cementless[1] + rbeta(iter, 29, 615)*weights$age_65$cementless[2],
+        year2 = rbeta(iter, 10, 369)*weights$age_65$cementless[1] + rbeta(iter, 2, 149)*weights$age_65$cementless[2],
+        year3 = rbeta(iter, 4, 204)*weights$age_65$cementless[1] + rbeta(iter, 0, 31)*weights$age_65$cementless[2],
+        year4 = rbeta(iter, 1, 88)*weights$age_65$cementless[1] + rbeta(iter, 0, 47)*weights$age_65$cementless[2],
+        year5 = rbeta(iter, 1, 88)*weights$age_65$cementless[1] + rbeta(iter, 0, 100)*weights$age_65$cementless[2]
       ),
       THA_cemented = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 101, 3789),
+        year2 = rbeta(iter, 9, 942),
+        year3 = rbeta(iter, 4, 632),
+        year4 = rbeta(iter, 1, 310),
+        year5 = rbeta(iter, 3, 366)
       ),
       THA_cementless = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 120, 3119),
+        year2 = rbeta(iter, 6, 587),
+        year3 = rbeta(iter, 5, 464),
+        year4 = rbeta(iter, 1, 210),
+        year5 = rbeta(iter, 1, 210)
       )
       ),
     
     age_75 = list(
       HA_cemented = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 218, 11238)*weights$age_75$cemented[1] + rbeta(iter, 135, 6311)*weights$age_75$cemented[2],
+        year2 = rbeta(iter, 21, 2945)*weights$age_75$cemented[1] + rbeta(iter, 9, 1308)*weights$age_75$cemented[2],
+        year3 = rbeta(iter, 8, 1151)*weights$age_75$cemented[1] + rbeta(iter, 3, 752)*weights$age_75$cemented[2],
+        year4 = rbeta(iter, 6, 1188)*weights$age_75$cemented[1] + rbeta(iter, 1, 457)*weights$age_75$cemented[2],
+        year5 = rbeta(iter, 6, 1188)*weights$age_75$cemented[1] + rbeta(iter, 1, 252)*weights$age_75$cemented[2]
       ),
       HA_cementless = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 83, 2418)*weights$age_75$cementless[1] + rbeta(iter, 42, 1275)*weights$age_75$cementless[2],
+        year2 = rbeta(iter, 6, 587)*weights$age_75$cementless[1] + rbeta(iter, 3, 310)*weights$age_75$cementless[2],
+        year3 = rbeta(iter, 3, 372)*weights$age_75$cementless[1] + rbeta(iter, 1, 157)*weights$age_75$cementless[2],
+        year4 = rbeta(iter, 2, 264)*weights$age_75$cementless[1] + rbeta(iter, 1, 153)*weights$age_75$cementless[2],
+        year5 = rbeta(iter, 2, 256)*weights$age_75$cementless[1] + rbeta(iter, 0, 38)*weights$age_75$cementless[2]
       ),
       THA_cemented = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 87, 2917),
+        year2 = rbeta(iter, 4, 627),
+        year3 = rbeta(iter, 2, 404),
+        year4 = rbeta(iter, 0, 135),
+        year5 = rbeta(iter, 1, 237)
       ),
       THA_cementless = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 69, 1707),
+        year2 = rbeta(iter, 6, 440),
+        year3 = rbeta(iter, 0, 34),
+        year4 = rbeta(iter, 0, 94),
+        year5 = rbeta(iter, 1, 183)
       )
     ),
     
     age_85 = list(
       HA_cemented = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 160, 12162)*weights$age_85$cemented[1] + rbeta(iter, 79, 4857)*weights$age_85$cemented[2],
+        year2 = rbeta(iter, 2, 1223)*weights$age_85$cemented[1] + rbeta(iter, 1, 624)*weights$age_85$cemented[2],
+        year3 = rbeta(iter, 1, 612)*weights$age_85$cemented[1] + rbeta(iter, 5, 752)*weights$age_85$cemented[2],
+        year4 = rbeta(iter, 2, 849)*weights$age_85$cemented[1] + rbeta(iter, 0, 126)*weights$age_85$cemented[2],
+        year5 = rbeta(iter, 0, 312)*weights$age_85$cemented[1] + rbeta(iter, 0, 100)*weights$age_85$cemented[2]
       ),
       HA_cementless = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 36, 1746)*weights$age_85$cementless[1] + rbeta(iter, 28, 952)*weights$age_85$cementless[2],
+        year2 = rbeta(iter, 2, 387)*weights$age_85$cementless[1] + rbeta(iter, 0, 31)*weights$age_85$cementless[2],
+        year3 = rbeta(iter, 0, 135)*weights$age_85$cementless[1] + rbeta(iter, 2, 178)*weights$age_85$cementless[2],
+        year4 = rbeta(iter, 0, 100)*weights$age_85$cementless[1] + rbeta(iter, 1, 109)*weights$age_85$cementless[2],
+        year5 = rbeta(iter, 1, 168)*weights$age_85$cementless[1] + rbeta(iter, 0, 44)*weights$age_85$cementless[2]
       ),
       THA_cemented = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 35, 1006),
+        year2 = rbeta(iter, 1, 183),
+        year3 = rbeta(iter, 1, 153),
+        year4 = rbeta(iter, 2, 142),
+        year5 = rbeta(iter, 0, 66)
       ),
       THA_cementless = list(
-        year1 = function() 1,
-        year2 = function() 1,
-        year3 = function() 1,
-        year4 = function() 1,
-        year5 = function() 1
+        year1 = rbeta(iter, 33, 572),
+        year2 = rbeta(iter, 0, 100),
+        year3 = rbeta(iter, 0, 55),
+        year4 = rbeta(iter, 0, 53),
+        year5 = rbeta(iter, 0, 100)
       )
     )
   )
 )
 
-utilities <- list()
+#Dislocation should only be assumed to contribute to QALY equivalent to wait times.
+utilities <- list(
+  
+  dislocation = list(
+    year1 = rbeta(iter, 9.77, 15.95),
+    year2 = rbeta(iter, 9.77, 15.95)/1+discount,
+    year3 = rbeta(iter, 9.77, 15.95)/(1+discount)^2,
+    year4 = rbeta(iter, 9.77, 15.95)/(1+discount)^3,
+    year5 = rbeta(iter, 9.77, 15.95)/(1+discount)^4
+  ),
+  
+  revision = list(
+    year1 = rbeta(iter, 36.94, 68.59),
+    year2 = rbeta(iter, 36.94, 68.59)/1+discount,
+    year3 = rbeta(iter, 36.94, 68.59)/(1+discount)^2,
+    year4 = rbeta(iter, 36.94, 68.59)/(1+discount)^3,
+    year5 = rbeta(iter, 36.94, 68.59)/(1+discount)^4
+  ),
+  
+  HA_cemented = list(
+    year1 = rbeta(iter, 95.3001, 63.5334),
+    year2 = rbeta(iter, 97.97326, 50.47107)/(1+discount),
+    year3 = rbeta(iter, 97.97326, 50.47107)/(1+discount)^2,
+    year4 = rbeta(iter, 97.97326, 50.47107)/(1+discount)^3,
+    year5 = rbeta(iter, 97.97326, 50.47107)/(1+discount)^4
+  ),
+  
+  HA_cementless = list(
+    year1 = rbeta(iter, 8.044704, 6.852896),
+    year2 = rbeta(iter, 48.65102, 35.23005)/(1+discount),
+    year3 = rbeta(iter, 48.65102, 35.23005)/(1+discount)^2,
+    year4 = rbeta(iter, 48.65102, 35.23005)/(1+discount)^3,
+    year5 = rbeta(iter, 48.65102, 35.23005)/(1+discount)^4
+  ),
+  
+  THA_cemented = list(
+    year1 = rbeta(iter, 5575.439, 2168.226),
+    year2 = rbeta(iter, 5468.776, 1921.462)/(1+discount),
+    year3 = rbeta(iter, 5468.776, 1921.462)/(1+discount)^2,
+    year4 = rbeta(iter, 5468.776, 1921.462)/(1+discount)^3,
+    year5 = rbeta(iter, 5468.776, 1921.462)/(1+discount)^4
+  ),
+  
+  THA_cementless = list(
+    year1 = rbeta(iter, 5615.287, 2293.568),
+    year2 = rbeta(iter, 5526.679, 2044.114)/(1+discount),
+    year3 = rbeta(iter, 5526.679, 2044.114)/(1+discount)^2,
+    year4 = rbeta(iter, 5526.679, 2044.114)/(1+discount)^3,
+    year5 = rbeta(iter, 5526.679, 2044.114)/(1+discount)^4
+  )
+)
 
 costs <- list()
--- 1_Model.R
+++ 1_Model.R
@@ -2,5 +2,6 @@ gc()
 
 #Basic model structure
 iter <- 10000 #define number of iterations
+discount <- 0.03 #define discount rate
 set.seed(888) #repeatability
-source("./0_Parameters.R") #call parameter vectors
+source("./0_Parameters.R") #call parameter vectors using iter 
