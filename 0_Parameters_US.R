8aefe06f8531c7a24aeb9883183e312cf624d7af
Robin Blythe <robin.blythe@qut.edu.au>
2024-09-09 19:15:04 +0800

    Updated data from Mita and added uncertainty

--- 0_Parameters_US.R
+++ 0_Parameters_US.R
@@ -3,18 +3,18 @@
 
 # Number of patients in each group, US data
 prev <- list(
-  n_65_HA_cemented = 447,
-  n_65_HA_cementless = 705,
-  n_65_THA_cemented = 106,
-  n_65_THA_cementless = 656,
-  n_75_HA_cemented = 1395,
-  n_75_HA_cementless = 1667,
-  n_75_THA_cemented = 131,
-  n_75_THA_cementless = 563,
-  n_85_HA_cemented = 2148,
-  n_85_HA_cementless = 1991,
-  n_85_THA_cemented = 121,
-  n_85_THA_cementless = 281
+  n_65_HA_cemented = 565,
+  n_65_HA_cementless = 925,
+  n_65_THA_cemented = 138,
+  n_65_THA_cementless = 815,
+  n_75_HA_cemented = 1489,
+  n_75_HA_cementless = 1847,
+  n_75_THA_cemented = 169,
+  n_75_THA_cementless = 649,
+  n_85_HA_cemented = 2279,
+  n_85_HA_cementless = 2077,
+  n_85_THA_cemented = 131,
+  n_85_THA_cementless = 255
 )
 
 # Initial generic measures
@@ -25,57 +25,35 @@ type_surg <- rdirichlet(iter, c(0.55, 0.24, 0.21))
 # All procedure costs are now cost/minute * number of minutes + add-ons (e.g. cement)
 cost_min_OT <- rgamma(iter, shape = 2.030, scale = 22.675) / (1 + discount)^3
 
-# Revision surgery cost from https://doi.org/10.1016/j.arth.2023.04.050
-# Recreate cost distribution
-base_cost_revisions <- c(rep(17686, 417), 
-                         rep(11125, 145), 
-                         rep(9066, 58), 
-                         rep(16553, 53), 
-                         rep(18374, 30), 
-                         rep(8677, 28), 
-                         rep(8427, 26), 
-                         rep(12743, 11),
-                         rep(13375, 7),
-                         rep(10710, 6),
-                         rep(10474, 6),
-                         rep(13244, 3),
-                         9745,
-                         15360, 
-                         10427)
-
-
 generic <- list(
 
   # Procedure costs = cost per minute times number of minutes. Mean duration so use rnorm
-  cost_HA = cost_min_OT * rnorm(iter, 78.45, 0.385),
-  cost_THA = cost_min_OT * rnorm(iter, 92.93, 0.891),
+  cost_HA = cost_min_OT * rnorm(iter, 78.400, 0.384),
+  cost_THA = cost_min_OT * rnorm(iter, 92.682, 0.887),
+  cost_revision_HA = cost_min_OT * rnorm(iter, 114.805, 1.624),
+  cost_revision_THA = cost_min_OT * rnorm(iter, 126.609, 4.865),
 
   # Cost of cementing
   # Extra theatre time from original study, https://doi.org/10.1002/14651858.CD001706.pub4
   cost_cementing = rgamma(iter, shape = 32.478, scale = 0.223) * # Number of minutes taken to apply cement
     cost_min_OT + # Cost per minute
-    (181 * 2 + runif(iter, min = 85, max = 115)) / (1 + discount)^5, # 2024 cost of cementing products, 2 packs antibiotic cement plus mixing tower
+    (181 * 2 + runif(iter, min = 85, max = 115)), # 2024 cost of cementing products, 2 packs antibiotic cement plus mixing tower
 
   cost_cementless = (type_surg[, 2] * 53 + type_surg[, 3] * 53 * 2) / (1 + discount)^1, # cost of cementless screws, 2024
 
   # HA is a weighted average, 65% bipolar, 35% monopolar
-  cost_HA_prosthesis = (0.65 * rtri(iter, min = 1715, mode = 2385, max = 3066) + # Bipolar
-    0.35 * rtri(iter, min = 1326, mode = 1658, max = 1991)) / (1 + discount)^1, # Monopolar
+  cost_HA_prosthesis = (0.65 * rnorm(iter, mean = 2390.5, sd = 262.246) + # Bipolar
+    0.35 * rnorm(iter, mean = 1658.5, sd = 129.085)) / (1 + discount)^1, # Monopolar
 
   # THA assumes ceramic head, same price for cemented and cementless prostheses
   cost_THA_prosthesis = list(
-    cemented = rtri(iter, min = 3965, mode = 4388, max = 4811),
-    cementless = rtri(iter, min = 4003, mode = 4776, max = 6375) / (1 + discount)^3),
-
-  
-  # Taking the top 8 indications leads to a median of 
-  cost_revision = rnorm(iter, mean = mean(base_cost_revisions), sd = sd(base_cost_revisions)/sqrt(length(base_cost_revisions))),
-
+    cemented = rnorm(iter, mean = 4388, sd = 164.219),
+    cementless = rgamma(iter, shape = 123.206, scale = 41.484) / (1 + discount)^3),
 
   # Dislocation outcomes from https://doi.org/10.1016/j.arth.2018.05.015
   # Turning costs from normal to gamma using https://doi.org/10.31219/osf.io/zf62e
   # Study data is from 2018 so bring it up to $2019
-  cost_dislocation_1year = rgamma(iter, shape = 9.55337, scale = 1819.09726) * (1 + discount),
+  cost_dislocation_1year = rgamma(iter, shape = 9.553, scale = 1819.097) * (1 + discount),
 
   # Dislocation rates from original study
   dislocation_rate = list(
@@ -86,8 +64,8 @@ generic <- list(
   ),
   dislocation_utility = rbeta(iter, 9.77, 15.95),
   revision_utility = rbeta(iter, 36.94, 68.59),
-  stable_HA_cemented_utility_followup = rbeta(iter, 97.97326, 50.47107),
-  stable_HA_cementless_utility_followup = rbeta(iter, 48.65102, 35.23005),
+  stable_HA_cemented_utility_followup = rbeta(iter, 97.973, 50.471),
+  stable_HA_cementless_utility_followup = rbeta(iter, 48.651, 35.230),
   stable_THA_cemented_utility_followup = rbeta(iter, 5468.776, 1921.462),
   stable_THA_cementless_utility_followup = rbeta(iter, 5526.679, 2044.114)
 )
@@ -97,28 +75,28 @@ generic <- list(
 # For values at or near 0, use an approximation: Beta(100, 100000) which is ~0.001
 transitions <- list(
 
-  # Mortality rate UPDATED
+  # Mortality rate
   death = list(
     age_65 = list(
-      year1 = rbeta(iter, 258389.8435, 3405003.936),
-      year2 = rbeta(iter, 254561.8513, 3408832.008),
-      year3 = rbeta(iter, 166517.9514, 3496876.976),
-      year4 = rbeta(iter, 15311.98842, 3648081.256),
-      year5 = rbeta(iter, 5742.004799, 3657657.102)
+      year1 = rbeta(iter, 402765.061, 5555716.843),
+      year2 = rbeta(iter, 446703.229, 5511780.825),
+      year3 = rbeta(iter, 283156.148, 5675327.965),
+      year4 = rbeta(iter, 46379.094, 5912113.994),
+      year5 = rbeta(iter, 12205.009, 5946280.501)
     ),
     age_75 = list(
-      year1 = rbeta(iter, 1682688.309, 12424850.29),
-      year2 = rbeta(iter, 1506154.818, 12601370.11),
-      year3 = rbeta(iter, 1190652.136, 12916885.47),
-      year4 = rbeta(iter, 153996.3315, 13953570.1),
-      year5 = 0 # No deaths?
+      year1 = rbeta(iter, 1855495.430, 15375290.994),
+      year2 = rbeta(iter, 1838894.241, 15391918.387),
+      year3 = rbeta(iter, 1515113.661, 15715672.114),
+      year4 = rbeta(iter, 232456.400, 16998374.252),
+      year5 = rbeta(iter, 8301.999, 17222497.531)
     ),
     age_85 = list(
-      year1 = rbeta(iter, 3700913.392, 16919758.65),
-      year2 = rbeta(iter, 2897156.095, 17723511.35),
-      year3 = rbeta(iter, 2047992.198, 18572700.86),
-      year4 = rbeta(iter, 231590.2987, 20389028.23),
-      year5 = rbeta(iter, 9081.998598, 20611596.89)
+      year1 = rbeta(iter, 3881878.849, 18528870.513),
+      year2 = rbeta(iter, 3309066.057, 19101690.330),
+      year3 = rbeta(iter, 2371734.983, 20039030.295),
+      year4 = rbeta(iter, 364519.064, 22046302.395),
+      year5 = rbeta(iter, 14202.001, 22396554.447)
     )
   ),
 
@@ -154,96 +132,96 @@ transitions <- list(
     )
   ),
 
-  # Revision rate UPDATED TO US DATA
+  # Revision rate
   revision = list(
     age_65 = list(
       HA_cemented = list(
-        year1 = rbeta(iter, 4022.98063, 195785.0572),
-        year2 = rbeta(iter, 3128.984212, 196679.0071),
-        year3 = rbeta(iter, 446.9977653, 199361.0002),
-        year4 = 0,
-        year5 = rbeta(iter, 446.9977653, 199361.0002)
+        year1 = rbeta(iter, 5075.986, 313019.131),
+        year2 = rbeta(iter, 5075.986, 313019.131),
+        year3 = rbeta(iter, 1, 10000),
+        year4 = rbeta(iter, 563.999, 317531.351),
+        year5 = rbeta(iter, 563.999, 317531.351)
       ),
       HA_cementless = list(
-        year1 = rbeta(iter, 19739.95872, 477284.0013),
-        year2 = rbeta(iter, 4229.989466, 492793.7727),
-        year3 = rbeta(iter, 1409.997662, 495614.1839),
-        year4 = rbeta(iter, 1409.997662, 495614.1839),
-        year5 = rbeta(iter, 1409.997662, 495614.1839)
+        year1 = rbeta(iter, 26824.961, 828798.786),
+        year2 = rbeta(iter, 12949.986, 842674.122),
+        year3 = rbeta(iter, 4624.989, 850997.990),
+        year4 = rbeta(iter, 1849.996, 853773.155),
+        year5 = rbeta(iter, 3699.993, 851923.428)
       ),
       THA_cemented = list(
-        year1 = rbeta(iter, 211.981138, 11023.01919),
-        year2 = rbeta(iter, 211.981138, 11023.01919),
-        year3 = 0,
-        year4 = 0,
-        year5 = 0
+        year1 = rbeta(iter, 273.985, 18494.012),
+        year2 = rbeta(iter, 273.985, 18494.012),
+        year3 = rbeta(iter, 1, 10000),
+        year4 = rbeta(iter, 1, 10000),
+        year5 = rbeta(iter, 1, 10000)
       ),
       THA_cementless = list(
-        year1 = rbeta(iter, 16399.96538, 413935.1263),
-        year2 = rbeta(iter, 2623.994514, 427711.1041),
-        year3 = rbeta(iter, 2623.994514, 427711.1041),
-        year4 = 0,
-        year5 = rbeta(iter, 655.9975232, 429678.3901)
+        year1 = rbeta(iter, 23634.966, 640589.075),
+        year2 = rbeta(iter, 5704.987, 658518.522),
+        year3 = rbeta(iter, 4074.996, 660149.290),
+        year4 = rbeta(iter, 10, 10000),
+        year5 = rbeta(iter, 815.001, 663410.394)
       )
     ),
     age_75 = list(
       HA_cemented = list(
-        year1 = rbeta(iter, 30689.99116, 1915334.451),
-        year2 = rbeta(iter, 5579.984955, 1940439.777),
-        year3 = rbeta(iter, 8369.993462, 1937653.472),
-        year4 = rbeta(iter, 1395.004476, 1944636.181),
-        year5 = rbeta(iter, 1395.004476, 1944636.181)
+        year1 = rbeta(iter, 32736.016, 2181409.054),
+        year2 = rbeta(iter, 11904.015, 2202242.765),
+        year3 = rbeta(iter, 5952.012, 2208196.343),
+        year4 = rbeta(iter, 4463.994, 2209677.158),
+        year5 = rbeta(iter, 1488.008, 2212667.252)
       ),
       HA_cementless = list(
-        year1 = rbeta(iter, 75014.9564, 2703872.426),
-        year2 = rbeta(iter, 30005.96173, 2748879.502),
-        year3 = rbeta(iter, 20003.99067, 2758883.709),
-        year4 = rbeta(iter, 5000.998418, 2773887.079),
-        year5 = 0
+        year1 = rbeta(iter, 88512.044, 3311825.646),
+        year2 = rbeta(iter, 40568.048, 3359771.930),
+        year3 = rbeta(iter, 31347.977, 3368985.589),
+        year4 = rbeta(iter, 5532.023, 3394818.027),
+        year5 = rbeta(iter, 1843.992, 3398477.039)
       ),
       THA_cemented = list(
-        year1 = rbeta(iter, 261.9847299, 16898.01505),
-        year2 = rbeta(iter, 130.9923572, 17029.00641),
-        year3 = 0,
-        year4 = 0,
-        year5 = 0
+        year1 = rbeta(iter, 337.988, 28222.012),
+        year2 = rbeta(iter, 168.994, 28391.005),
+        year3 = rbeta(iter, 10, 10000),
+        year4 = rbeta(iter, 10, 10000),
+        year5 = rbeta(iter, 10, 10000)
       ),
       THA_cementless = list(
-        year1 = rbeta(iter, 8444.971064, 308522.943),
-        year2 = rbeta(iter, 1688.994903, 315279.0486),
-        year3 = rbeta(iter, 562.9989436, 316405.4124),
-        year4 = 0,
-        year5 = 0
+        year1 = rbeta(iter, 12330.968, 408868.938),
+        year2 = rbeta(iter, 2595.996, 418604.316),
+        year3 = rbeta(iter, 648.999, 420551.455),
+        year4 = rbeta(iter, 10, 10000),
+        year5 = rbeta(iter, 10, 10000)
       )
     ),
     age_85 = list(
       HA_cemented = list(
-        year1 = rbeta(iter, 81623.92815, 4532276.019),
-        year2 = rbeta(iter, 12887.95428, 4600999.659),
-        year3 = rbeta(iter, 4296.008692, 4609617.31),
-        year4 = 0,
-        year5 = 0
+        year1 = rbeta(iter, 88725.094, 5086905.419),
+        year2 = rbeta(iter, 15924.979, 5159693.058),
+        year3 = rbeta(iter, 6825.013, 5168809.894),
+        year4 = rbeta(iter, 10, 10000),
+        year5 = rbeta(iter, 10, 10000)
       ),
       HA_cementless = list(
-        year1 = rbeta(iter, 93576.92565, 3870500.929),
-        year2 = rbeta(iter, 27874.03801, 3936212.362),
-        year3 = rbeta(iter, 23891.93647, 3940178.555),
-        year4 = rbeta(iter, 3982.009343, 3960108.456),
-        year5 = rbeta(iter, 1991.002133, 3962094.013)
+        year1 = rbeta(iter, 103550.019, 4185491.760),
+        year2 = rbeta(iter, 33135.915, 4255894.038),
+        year3 = rbeta(iter, 20709.969, 4268324.681),
+        year4 = rbeta(iter, 8284.001, 4280757.565),
+        year5 = rbeta(iter, 2070.996, 4286961.931)
       ),
       THA_cemented = list(
-        year1 = rbeta(iter, 120.9917201, 14519.00643),
-        year2 = 0,
-        year3 = 0,
-        year4 = 0,
-        year5 = 0
+        year1 = rbeta(iter, 130.992, 17029.006),
+        year2 = rbeta(iter, 10, 10000),
+        year3 = rbeta(iter, 10, 10000),
+        year4 = rbeta(iter, 10, 10000),
+        year5 = rbeta(iter, 10, 10000)
       ),
       THA_cementless = list(
-        year1 = rbeta(iter, 561.9930449, 78398.03001),
-        year2 = rbeta(iter, 561.9930449, 78398.03001),
-        year3 = rbeta(iter, 280.9964006, 78678.9913),
-        year4 = rbeta(iter, 1, 100000),
-        year5 = rbeta(iter, 1, 100000)
+        year1 = rbeta(iter, 10, 10000),
+        year2 = rbeta(iter, 254.996, 64768.979),
+        year3 = rbeta(iter, 509.992, 64514.013),
+        year4 = rbeta(iter, 10, 10000),
+        year5 = rbeta(iter, 10, 10000)
       )
     )
   )
@@ -340,70 +318,70 @@ costs <- list(
   # Assume revision leads to same surgery type as initial procedure
   cost_revision = list(
     HA_cemented = list(
-      year1 = (generic$cost_revision +
+      year1 = (generic$cost_revision_HA +
         generic$cost_cementing +
         generic$cost_HA_prosthesis),
-      year2 = (generic$cost_revision +
+      year2 = (generic$cost_revision_HA +
         generic$cost_cementing +
         generic$cost_HA_prosthesis) / (1 + discount),
-      year3 = (generic$cost_revision +
+      year3 = (generic$cost_revision_HA +
         generic$cost_cementing +
         generic$cost_HA_prosthesis) / (1 + discount)^2,
-      year4 = (generic$cost_revision +
+      year4 = (generic$cost_revision_HA +
         generic$cost_cementing +
         generic$cost_HA_prosthesis) / (1 + discount)^3,
-      year5 = (generic$cost_revision +
+      year5 = (generic$cost_revision_HA +
         generic$cost_cementing +
         generic$cost_HA_prosthesis) / (1 + discount)^4
     ),
     HA_cementless = list(
-      year1 = (generic$cost_revision +
+      year1 = (generic$cost_revision_HA +
         generic$cost_cementless +
         generic$cost_HA_prosthesis),
-      year2 = (generic$cost_revision +
+      year2 = (generic$cost_revision_HA +
         generic$cost_cementless +
         generic$cost_HA_prosthesis) / (1 + discount),
-      year3 = (generic$cost_revision +
+      year3 = (generic$cost_revision_HA +
         generic$cost_cementless +
         generic$cost_HA_prosthesis) / (1 + discount)^2,
-      year4 = (generic$cost_revision +
+      year4 = (generic$cost_revision_HA +
         generic$cost_cementless +
         generic$cost_HA_prosthesis) / (1 + discount)^3,
-      year5 = (generic$cost_revision +
+      year5 = (generic$cost_revision_HA +
         generic$cost_cementless +
         generic$cost_HA_prosthesis) / (1 + discount)^4
     ),
     THA_cemented = list(
-      year1 = (generic$cost_revision +
+      year1 = (generic$cost_revision_THA +
         generic$cost_cementing +
         generic$cost_THA_prosthesis$cemented),
-      year2 = (generic$cost_revision +
+      year2 = (generic$cost_revision_THA +
         generic$cost_cementing +
         generic$cost_THA_prosthesis$cemented) / (1 + discount),
-      year3 = (generic$cost_revision +
+      year3 = (generic$cost_revision_THA +
         generic$cost_cementing +
         generic$cost_THA_prosthesis$cemented) / (1 + discount)^2,
-      year4 = (generic$cost_revision +
+      year4 = (generic$cost_revision_THA +
         generic$cost_cementing +
         generic$cost_THA_prosthesis$cemented) / (1 + discount)^3,
-      year5 = (generic$cost_revision +
+      year5 = (generic$cost_revision_THA +
         generic$cost_cementing +
         generic$cost_THA_prosthesis$cemented) / (1 + discount)^4
     ),
     THA_cementless = list(
-      year1 = (generic$cost_revision +
+      year1 = (generic$cost_revision_THA +
         generic$cost_cementless +
         generic$cost_THA_prosthesis$cementless),
-      year2 = (generic$cost_revision +
+      year2 = (generic$cost_revision_THA +
         generic$cost_cementless +
         generic$cost_THA_prosthesis$cementless) / (1 + discount),
-      year3 = (generic$cost_revision +
+      year3 = (generic$cost_revision_THA +
         generic$cost_cementless +
         generic$cost_THA_prosthesis$cementless) / (1 + discount)^2,
-      year4 = (generic$cost_revision +
+      year4 = (generic$cost_revision_THA +
         generic$cost_cementless +
         generic$cost_THA_prosthesis$cementless) / (1 + discount)^3,
-      year5 = (generic$cost_revision +
+      year5 = (generic$cost_revision_THA +
         generic$cost_cementless +
         generic$cost_THA_prosthesis$cementless) / (1 + discount)^4
     )
